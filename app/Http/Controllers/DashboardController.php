985d854e5acadb63ff38892d953af5ff6604210f
yantoww <hardiantobangkit@gmail.com>
2025-08-25 00:20:09 +0700

    event dashboard member sudah nyambung database

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,10 +9,10 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // Ambil data event, bisa pakai paginate biar sesuai layout
+        
         $events = Event::latest()->paginate(6);
 
-        // Kirim ke view
+      
         return view('member.dashboard.index', compact('events'));
     }
 }
--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -8,14 +8,14 @@ use Illuminate\Support\Facades\Auth;
 
 class HistoryController extends Controller
 {
-    // Index: tampil semua event
+   
     public function index()
     {
         $histories = Event::paginate(5);
         return view('member.history.index', compact('histories'));
     }
 
-    // Show: detail 1 event + user login
+  
     public function show($id)
     {
         $event = Event::findOrFail($id);
