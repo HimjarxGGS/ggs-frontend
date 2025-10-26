1791c268abba24f29bbbed143e063d734ec6a425
Farid | Iamjustzero <imjustzero@duck.com>
2025-10-26 12:51:13 +0700

    adjust volunteer, event, impact one and two counter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
 
 document.addEventListener("DOMContentLoaded", () => {
+    // Hamburger menu code
     const toggleBtn = document.getElementById("hamburgerBtn");
     const mobileMenu = document.getElementById("mobileMenu");
     const navbar = document.querySelector("nav");
@@ -11,13 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
         mobileMenu.classList.toggle("hidden");
     });
 
-
     // close mobile menu outside
     document.addEventListener("click", (event) => {
         const isClickInsideMenu = mobileMenu.contains(event.target);
         const isClickToggle = toggleBtn.contains(event.target);
 
-        if (!mobileMenu.classList.contains("hidden") &&
+        if (
+            !mobileMenu.classList.contains("hidden") &&
             !isClickInsideMenu &&
             !isClickToggle
         ) {
@@ -31,58 +32,54 @@ document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("scroll", () => {
         if (window.scrollY > 10) {
             navbar.classList.add("backdrop-blur-3xl");
-            navbar.classList.remove("bg-[#F2F2F7]", "shadow-md"); 
+            navbar.classList.remove("bg-[#F2F2F7]", "shadow-md");
         } else {
             navbar.classList.remove("backdrop-blur-md");
         }
     });
-});
 
-document.addEventListener("DOMContentLoaded", () => {
     function countUp(el, target, duration) {
         let start = 0;
-        let increment = target / (duration / 50); 
+        let increment = target / (duration / 50);
         let interval = setInterval(() => {
             start += increment;
             if (start >= target) {
                 start = target;
                 clearInterval(interval);
-                el.textContent = Math.floor(start) + "+";
+                el.textContent = Math.floor(start) + " + ";
             } else {
                 el.textContent = Math.floor(start);
             }
         }, 70);
     }
 
-    countUp(document.getElementById("volunteerCount"), 100, 3000); 
-    countUp(document.getElementById("eventCount"), 50, 3000);
-    countUp(document.getElementById("dampak1"), 1000, 3000);
-    countUp(document.getElementById("dampak2"), 8000, 5000);
+    countUp(document.getElementById("volunteerCount"), 880, 3000);
+    countUp(document.getElementById("eventCount"), 11, 3000);
+    countUp(document.getElementById("dampak1"), 155, 3000);
+    countUp(document.getElementById("dampak2"), 303, 5000);
+
+    // File input code
+    const dropPoster = document.getElementById("dropPoster");
+    const fileInput = document.getElementById("poster");
+
+    dropPoster.addEventListener("click", () => fileInput.click());
+
+    dropPoster.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        dropPoster.classList.add("bg-gray-200");
+    });
+
+    dropPoster.addEventListener("dragleave", () => {
+        dropPoster.classList.remove("bg-gray-200");
+    });
+
+    dropPoster.addEventListener("drop", (e) => {
+        e.preventDefault();
+        dropPoster.classList.remove("bg-gray-200");
+
+        if (e.dataTransfer.files.length > 0) {
+            fileInput.files = e.dataTransfer.files;
+            console.log("File dropped:", fileInput.files[0]);
+        }
+    });
 });
-
-const dropPoster = document.getElementById("dropPoster");
-const fileInput = document.getElementById("poster");
-
-  
-  dropPoster.addEventListener("click", () => fileInput.click());
-
-  
-  dropPoster.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    dropPoster.classList.add("bg-gray-200");
-  });
-
-  dropPoster.addEventListener("dragleave", () => {
-    dropPoster.classList.remove("bg-gray-200");
-  });
-
-
-  dropPoster.addEventListener("drop", (e) => {
-    e.preventDefault();
-    dropPoster.classList.remove("bg-gray-200");
-
-    if (e.dataTransfer.files.length > 0) {
-      fileInput.files = e.dataTransfer.files;
-      console.log("File dropped:", fileInput.files[0]);
-    }
-  });
--- resources/views/guest/events/index.blade.php
+++ resources/views/guest/events/index.blade.php
@@ -35,12 +35,11 @@
 
                     <!-- Text -->
                     <div class="text-left">
-                        <h2 id="volunteerCount" class="text-xl md:text-2xl font-bold text-palette-4" data-target="100">0
+                        <h2 id="volunteerCount" class="text-xl md:text-2xl font-bold text-palette-4" data-target="880">0
                         </h2>
                         <p class="text-gray-600 text-xs md:text-xs">Volunteer</p>
                     </div>
                 </div>
-
                 <!-- Item 2 -->
                 <div class="flex items-center md:gap-10 gap-4 pl-10 md:ml-10">
                     <!-- Icon pakai img -->
@@ -57,17 +56,16 @@
         </div>
     </section>
 
-<!-- upcoming event start -->
-<div class="flex justify-items-center max-w-7xl mx-auto">
-    <div class="mt-32 mb-12 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        @forelse ($upcoming as $i => $event)
-        <a href="{{ route('events.show', $event->id) }}"
-            class="block group {{ $i >= 1 ? 'hidden sm:block' : '' }}">
-            <div class="bg-white rounded-3xl shadow-md overflow-hidden hover:shadow-lg hover:shadow-gray-600 ease-in-out transition duration-500 flex flex-col h-full">
-                <div class="relative">
-                    <img src="{{ $event->poster ? asset('storage/'.$event->poster) : asset('images/gambar-event.png') }}"
-                        alt="Event Cover"
-                        class="w-full h-52 object-cover">
+    <!-- upcoming event start -->
+    <div class="flex justify-items-center max-w-7xl mx-auto">
+        <div class="mt-32 mb-12 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            @forelse ($upcoming as $i => $event)
+                <a href="{{ route('events.show', $event->id) }}" class="block group {{ $i >= 1 ? 'hidden sm:block' : '' }}">
+                    <div
+                        class="bg-white rounded-3xl shadow-md overflow-hidden hover:shadow-lg hover:shadow-gray-600 ease-in-out transition duration-500 flex flex-col h-full">
+                        <div class="relative">
+                            <img src="{{ $event->poster ? asset('storage/' . $event->poster) : asset('images/gambar-event.png') }}"
+                                alt="Event Cover" class="w-full h-52 object-cover">
 
                             <span
                                 class="absolute top-7 left-7 bg-green-500 text-white text-xs font-semibold px-3 py-1 rounded-full">
@@ -174,15 +172,16 @@
                     <img src="{{ asset('icons/tree.svg') }}" alt="Tree" class="w-8 h-8 md:w-10 md:h-10">
                 </div>
 
-            <!-- Card 2 -->
-            <div class="flex items-center justify-between bg-gray-200 rounded-2xl px-6 py-4 w-80 md:w-80">
-                <div class="text-left">
-                    <div class="flex">
-                        <h2 class="text-xl md:text-2xl font-bold text-palette-2" id="dampak2" data-target="5000">0</h2>
-                        <span class="text-sm ml-1 pt-2">kg</span>
+                <!-- Card 2 -->
+                <div class="flex items-center justify-between bg-gray-200 rounded-2xl px-6 py-4 w-80 md:w-80">
+                    <div class="text-left">
+                        <div class="flex">
+                            <h2 class="text-xl md:text-2xl font-bold text-palette-2" id="dampak2" data-target="5000">0
+                            </h2>
+                            <span class="text-sm ml-1 pt-2">kg</span>
+                        </div>
+                        <p class="text-xs md:text-sm text-palette-4">Sampah Dikelola</p>
                     </div>
-                    <p class="text-xs md:text-sm text-palette-4">Sampah Dikelola</p>
-                </div>
 
                     <img src="{{ asset('icons/recycle.svg') }}" alt="Recycle" class="w-8 h-8 md:w-10 md:h-10">
                 </div>
@@ -266,9 +265,9 @@
                                 class="absolute transition rounded-3xl opacity-25 -inset-1 bg-gradient-to-r from-palette-4 to-palette-2 blur duration-400 group-hover:opacity-100 group-hover:duration-500">
                             </div>
                             <div class="relative p-6 space-y-6 leading-none rounded-3xl bg-white ring-1 ring-gray-200">
-                                <div class="flex items-center space-x-4"><img src="{{ asset('images/testimoni_firda.png') }}"
-                                        alt="Firda" class="w-12 h-12 bg-center bg-cover border rounded-full"
-                                        alt="Parag Agrawal">
+                                <div class="flex items-center space-x-4"><img
+                                        src="{{ asset('images/testimoni_firda.png') }}" alt="Firda"
+                                        class="w-12 h-12 bg-center bg-cover border rounded-full" alt="Parag Agrawal">
                                     <div>
                                         <h3 class="text-lg font-semibold text-gray-800">Firda</h3>
                                         <p class="text-palette-4 text-md">Student</p>
