0ab603de80c0311bc86c4ba9809594986f031693
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-11-16 17:45:24 +0700

    data completion checker on registrasi event

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,57 +3,68 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\Pendaftar;
 use Illuminate\Support\Facades\Auth; // <- jangan lupa import
 
 class DashboardController extends Controller
 {
     public function index()
-{
-    $query = Event::query();
+    {
+        $query = Event::query();
 
-    // Search
-    if (request('search')) {
-        $query->where(function ($q) {
-            $q->where('name', 'like', '%' . request('search') . '%')
-              ->orWhere('description', 'like', '%' . request('search') . '%');
-        });
+        // Search
+        if (request('search')) {
+            $query->where(function ($q) {
+                $q->where('name', 'like', '%' . request('search') . '%')
+                    ->orWhere('description', 'like', '%' . request('search') . '%');
+            });
+        }
+
+        // Sorting
+        if (request('sort') == 'oldest') {
+            $query->orderBy('event_date', 'asc');
+        } else {
+            $query->orderBy('event_date', 'desc');
+        }
+
+        $events = $query->paginate(6)->appends(request()->query());
+        $users = Auth::user();
+
+        return view('member.dashboard.index', compact('events', 'users'));
     }
 
-    // Sorting
-    if (request('sort') == 'oldest') {
-        $query->orderBy('event_date', 'asc');
-    } else {
-        $query->orderBy('event_date', 'desc');
+    public function showMember($id)
+    {
+        $event = Event::findOrFail($id);
+
+        // Ambil 3 event lain
+        $moreEvents = Event::where('id', '!=', $event->id)
+            ->where('status', 'active')
+            ->latest()
+            ->take(3)
+            ->get();
+
+        // kalau mau pakai view khusus member:
+        return view('member.dashboard.detail', compact('event', 'moreEvents'));
     }
 
-    $events = $query->paginate(6)->appends(request()->query());
-    $users = Auth::user();
+    public function register($id)
+    {
 
-    return view('member.dashboard.index', compact('events', 'users'));
-}
-
- public function showMember($id)
-{
-    $event = Event::findOrFail($id);
-
-    // Ambil 3 event lain
-    $moreEvents = Event::where('id', '!=', $event->id)
-                        ->where('status', 'active')
-                        ->latest()
-                        ->take(3)
-                        ->get();
-
-    // kalau mau pakai view khusus member:
-     return view('member.dashboard.detail', compact('event', 'moreEvents'));
-
-   
-}
-
-public function register($id)
-{
-    $event = Event::findOrFail($id);
-    return view('member.dashboard.register', compact('event'));
-}
+        $user = Auth::user();
 
 
+        if ($user) {
+            $pendaftar = Pendaftar::where('user_id', '=', $user->id)->first();
+            
+            if (empty($pendaftar->nama_lengkap) || empty($pendaftar->date_of_birth) || empty($pendaftar->email) || empty($pendaftar->asal_instansi) || empty($pendaftar->no_telepon)) {
+                return redirect('/data-diri')
+                    ->with('error', 'Lengkapi data diri terlebih dahulu sebelum registrasi event.');
+            }
+        }
+
+
+        $event = Event::findOrFail($id);
+        return view('member.dashboard.register', compact('event'));
+    }
 }
--- app/Http/Controllers/MemberEventRegisterController.php
+++ app/Http/Controllers/MemberEventRegisterController.php
@@ -8,46 +8,45 @@ use Illuminate\Support\Facades\Auth;
 
 class MemberEventRegisterController extends Controller
 {
-   public function store(Request $request, $eventId)
-{
-    $request->validate([
-        'bukti_share' => 'required|file|mimes:jpg,png,jpeg,pdf|max:2048',
-        'bukti_payment' => 'required|file|mimes:jpg,png,jpeg,pdf|max:2048',
-        'pembayaran_via' => 'required|string',
-        'hadir' => 'required|in:ya,tidak',
-        'tata_tertib' => 'required|in:ya,tidak',
-    ]);
-
-    $buktiSharePath = $request->file('bukti_share')->store('bukti_share');
-    $buktiPaymentPath = $request->file('bukti_payment')->store('bukti_payment');
-
-    $pendaftar = Auth::user()->pendaftar;
-
-    if (!$pendaftar) {
-        return redirect()->back()->with('error', 'Lengkapi data diri terlebih dahulu sebelum mendaftar event.');
-    }
-
-    if (PendaftarEvent::where('event_id', $eventId)
-    ->where('pendaftar_id', $pendaftar->id)
-    ->exists()) 
+    public function store(Request $request, $eventId)
     {
-        return back()->with('error', 'Anda sudah mendaftar event ini.');
+        $request->validate([
+            'bukti_share' => 'required|file|mimes:jpg,png,jpeg,pdf|max:2048',
+            'bukti_payment' => 'required|file|mimes:jpg,png,jpeg,pdf|max:2048',
+            'pembayaran_via' => 'required|string',
+            'hadir' => 'required|in:ya,tidak',
+            'tata_tertib' => 'required|in:ya,tidak',
+        ]);
+
+        $buktiSharePath = $request->file('bukti_share')->store('bukti_share');
+        $buktiPaymentPath = $request->file('bukti_payment')->store('bukti_payment');
+
+        $pendaftar = Auth::user()->pendaftar;
+
+        if (!$pendaftar) {
+            return redirect()->back()->with('error', 'Lengkapi data diri terlebih dahulu sebelum mendaftar event.');
+        }
+
+        if (PendaftarEvent::where('event_id', $eventId)
+            ->where('pendaftar_id', $pendaftar->id)
+            ->exists()
+        ) {
+            return back()->with('error', 'Anda sudah mendaftar event ini.');
+        }
+
+
+        PendaftarEvent::create([
+            'event_id' => $eventId,
+            'pendaftar_id' => $pendaftar->id,
+            'status' => 'pending',
+            'bukti_share' => $buktiSharePath,
+            'bukti_payment' => $buktiPaymentPath,
+            'opsi_payment' => $request->pembayaran_via,
+            'kesediaan_hadir' => $request->hadir,
+            'kesediaan_menaati_aturan' => $request->tata_tertib,
+        ]);
+
+        return redirect()->route('member.dashboard.successEvent')
+            ->with('success', 'Registrasi event berhasil!');
     }
-
-
-    PendaftarEvent::create([
-        'event_id' => $eventId,
-        'pendaftar_id' => $pendaftar->id,
-        'status' => 'pending',
-        'bukti_share' => $buktiSharePath,
-        'bukti_payment' => $buktiPaymentPath,
-        'opsi_payment' => $request->pembayaran_via,
-        'kesediaan_hadir' => $request->hadir,
-        'kesediaan_menaati_aturan' => $request->tata_tertib,
-    ]);
-
-    return redirect()->route('member.dashboard.successEvent')
-                     ->with('success', 'Registrasi event berhasil!');
-}
-
 }
